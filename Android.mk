28c8dd88ba6649856b355e6564d7ea84ea696567
TheComputerGuy96 <vwestlife20@gmail.com>
2016-08-09 15:39:36 +0300

    Update for AOSP (Part 8)

--- Android.mk
+++ Android.mk
@@ -1,3 +1,7 @@
-ifeq ($(TARGET_DEVICE),kyleprods)
-    include $(all-subdir-makefiles)
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),kylepro)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
 endif
