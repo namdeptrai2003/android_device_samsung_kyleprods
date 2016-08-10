2486d801806b5415c98d38bd9ce1866298962721
TheComputerGuy96 <vwestlife20@gmail.com>
2016-08-10 15:09:16 +0300

    Update for AOSP (Part 16)

--- full_kyleprods.mk
+++ full_kyleprods.mk
@@ -5,11 +5,11 @@ PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/kylepro/device_kylepro.mk)
+$(call inherit-product, device/samsung/kyleprods/device_kyleprods.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := kylepro
-PRODUCT_NAME := full_kylepro
+PRODUCT_DEVICE := kyleprods
+PRODUCT_NAME := full_kyleprods
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := Samsung
-PRODUCT_MODEL := GT-S7580
+PRODUCT_MODEL := GT-S7582
