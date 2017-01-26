b40597842c9f0e38d2f2531fbbe9c4c539c54201
sostk <poudelswastik@gmail.com>
2017-01-26 23:40:01 +1100

    Update xosp.mk

--- xosp.mk
+++ xosp.mk
@@ -1,6 +1,5 @@
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/xosp/config/common_mini_phone.mk)
-$(call inherit-product, vendor/xosp/config/xosp.mk)
+$(call inherit-product, vendor/hexagon/config/common_mini_phone.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -10,7 +9,7 @@ $(call inherit-product, device/samsung/kyleprods/device_kyleprods.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := kyleprods
-PRODUCT_NAME := xosp_kyleprods
+PRODUCT_NAME := hexagon_kyleprods
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-S7582
